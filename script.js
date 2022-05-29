98e678f5200dce0785609dfae526c2ac87d0d1a3
Sarah Nuno <saritanu321@yahoo.com>
2022-05-29 12:52:29 -0700

    Program is now running with the addition of the fillState() and next_row() functions. Need to find a way to clear the canvas to be able to run more than one rule.

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ let newArr = [];
 
 function runRule() {
     // Grab the cell size
-    let cellWidth = document.querySelector('.cellSize');
+    let cellWidth = document.querySelector('.cellSize').value;
     let arrSize = canvas.width / cellWidth;
 
     //  Grab the rule number and convert to binary string
-    let ruleNum = document.querySelector('.ruleNum');
+    let ruleNum = document.querySelector('.ruleNum').value;
     let number = parseInt(ruleNum);
     let result = number.toString(2);
 
@@ -26,15 +26,66 @@ function runRule() {
     }
 
     initialRow(arrSize);
+
+    for (let i = 0; i < arr.length; i++) {
+        fillState(cellWidth);
+        generation++;
+        arr = next_row(rulesNew);     
+    }
+    
 }
 
-// woll initialize the first row - generation 0
+// will initialize the first row - generation 0
 function initialRow(arrSize) {
     for (let i = 0; i < arrSize; i++) {
         if(i == Math.floor(arrSize / 2)) arr.push(1);
         else arr.push(0);
     }
+    console.log(arr);
     return arr;
+}
+
+// will the fill the cells with correct color
+function fillState(cellWidth) {
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] == 1) c.fillStyle = 'black';
+        else c.fillStyle = 'white';
+        
+        c.fillRect(i*cellWidth, generation*cellWidth, cellWidth, cellWidth);
+    }
+    return arr;
+}
+
+// function to determine the states of the next row
+function next_row(rulesNew) {
+    for (let i = 0; i < arr.length; i++) {
+        let left = arr[i-1];
+        let current = arr[i];
+        let right = arr[i+1];
+
+        if(left == null) left = 0;
+        if(right == null) right = 0;
+        newArr.push(resultState(left, current, right, rulesNew));
+    }
+    // clear arr and replace with newArr values
+    arr.length = 0;
+    arr.push.apply(arr, newArr);
+    newArr.length = 0;
+
+    return arr;
+}
+
+// function to return the next cell state when the three parameter cells match the rule
+function resultState(left, current, right, rulesNew) {
+    if(left == 1 && current == 1 && right == 1) return rulesNew[0];
+    else if(left == 1 && current == 1 && right == 0) return rulesNew[1];
+    else if(left == 1 && current == 0 && right == 1) return rulesNew[2];
+    else if(left == 1 && current == 0 && right == 0) return rulesNew[3];
+    else if(left == 0 && current == 1 && right == 1) return rulesNew[4];
+    else if(left == 0 && current == 1 && right == 0) return rulesNew[5];
+    else if(left == 0 && current == 0 && right == 1) return rulesNew[6];
+    else if(left == 0 && current == 0 && right == 0) return rulesNew[7];
+    return 0;
 }
 
 // Event Listener to call the runRule() function when the run rule button is clicked
